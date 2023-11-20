8b91b1c8e5601cb913ac742b1face0927321e34b
tanaiadam <adam.tanai@gmail.com>
2023-11-20 10:32:15 +0100

    refactor

--- src/testsRefactor/MainRefactorTest.java
+++ src/testsRefactor/MainRefactorTest.java
@@ -1,41 +1,41 @@
 package testsRefactor;
 
-import main.Main;
-import main.Product;
 import org.junit.Test;
+import refactor.MainRefactor;
+import refactor.ProductRefactor;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainRefactorTest {
-    private ArrayList<Product> initTest(){
-        ArrayList<Product> products = new ArrayList<Product>();
-        products.add(new Product("Elso", 200, 20));
-        products.add(new Product("Masodik", 300, 30));
-        products.add(new Product("Harmadik", 400, 40));
-        products.add(new Product("Negyedik", 500, 50));
+    private ArrayList<ProductRefactor> initTest(){
+        ArrayList<ProductRefactor> products = new ArrayList<>();
+        products.add(new ProductRefactor("Elso", 200, 20));
+        products.add(new ProductRefactor("Masodik", 300, 30));
+        products.add(new ProductRefactor("Harmadik", 400, 40));
+        products.add(new ProductRefactor("Negyedik", 500, 50));
 
         return products;
     }
     @Test
     public void ZeroProductTest() {
-        ArrayList<Product> products = initTest();
+        ArrayList<ProductRefactor> products = initTest();
 
-        ArrayList<Product> testResult = Main.Evalutate(products);
-        ArrayList<Product> expectedResult = new ArrayList<>();
+        ArrayList<ProductRefactor> testResult = MainRefactor.EvalutateTop3(products);
+        ArrayList<ProductRefactor> expectedResult = new ArrayList<>();
 
         assertEquals(expectedResult, testResult);
     }
 
     @Test
     public void Only1ProductTest() {
-        ArrayList<Product> products = initTest();
+        ArrayList<ProductRefactor> products = initTest();
 
         products.get(2).incrementSold(2);
 
-        ArrayList<Product> testResult = Main.Evalutate(products);
-        ArrayList<Product> expectedResult = new ArrayList<>();
+        ArrayList<ProductRefactor> testResult = MainRefactor.EvalutateTop3(products);
+        ArrayList<ProductRefactor> expectedResult = new ArrayList<>();
         expectedResult.add(products.get(2));
 
         assertEquals(expectedResult, testResult);
@@ -44,18 +44,18 @@ public class MainRefactorTest {
 
     @Test
     public void AllProductOnlyGet3Test() {
-        ArrayList<Product> products = initTest();
+        ArrayList<ProductRefactor> products = initTest();
 
         products.get(0).incrementSold(2);
         products.get(1).incrementSold(1);
         products.get(2).incrementSold(2);
         products.get(3).incrementSold(1);
 
-        ArrayList<Product> testResult = Main.Evalutate(products);
-        ArrayList<Product> expectedResult = new ArrayList<>();
-        expectedResult.add(products.get(3));
-        expectedResult.add(products.get(1));
+        ArrayList<ProductRefactor> testResult = MainRefactor.EvalutateTop3(products);
+        ArrayList<ProductRefactor> expectedResult = new ArrayList<>();
         expectedResult.add(products.get(2));
+        expectedResult.add(products.get(3));
+        expectedResult.add(products.get(0));
 
         assertEquals(expectedResult, testResult);
     }
